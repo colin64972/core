6054fb70adb23ac6f76bec54cec9f8fd7435ae9e
cjo3 <56013989+cjo3@users.noreply.github.com>
2019-10-13 12:49:04 -0700

    add eslint to configs

---
+++ configs/eslint.js
@@ -0,0 +1,24 @@
+module.exports = setEslint = (
+  ecmaFeaturesJsx = false,
+  customExts = [],
+  customPlugs = [],
+  customEnvs = {}
+) => ({
+  parserOptions: {
+    sourceType: 'module',
+    ecmaFeatures: {
+      globalReturn: false,
+      impliedStrict: true,
+      jsx: ecmaFeaturesJsx,
+    },
+  },
+  extends: [...customExts, 'prettier'],
+  plugins: [...customPlugs, 'prettier'],
+  rules: {
+    'prettier/prettier': ['error'],
+  },
+  env: {
+    es2020: true,
+    ...customEnvs,
+  },
+})
