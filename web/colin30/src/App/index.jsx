5bcd23e738473f023ba45e8283070c78d1920256
colin64972 <56013989+colin64972@users.noreply.github.com>
2019-11-16 17:11:38 -0800

    add react code with eslint support

---
+++ web/colin30/src/App/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const App = () => (
+  <div>
+    <h1>Dolor Et Erat Sadipscing Sed Diam Takimata</h1>
+    <p>
+      Sed sit kasd est sea clita, clita rebum sit dolor sadipscing takimata
+      gubergren sadipscing. Stet ut et aliquyam est rebum clita takimata justo
+      est, lorem et dolor dolore takimata diam et ipsum et sanctus, amet dolores
+      sit sanctus labore eirmod sit, eos diam sed amet lorem dolore dolor diam
+      diam,.
+    </p>
+  </div>
+)
+
+export default App
---
+++ web/colin30/src/index.js
@@ -0,0 +1,5 @@
+import { render } from 'react-dom'
+import { createElement } from 'react'
+import App from './App'
+
+render(createElement(App), document.getElementById('app'))
