95587dfb9cf7c80b1c23472985abae5b14482bfe
colin30 <colin30@post.com>
2019-11-23 18:27:20 -0800

    add shared pug html template to webpack config

--- web/colin30/src/index.js
+++ web/colin30/src/index.js
@@ -2,6 +2,12 @@ import { render } from 'react-dom'
 import { createElement } from 'react'
 import App from './App'
 
-console.log('%c env', 'color: lightyellow; font-size: large', process.env)
+console.log(
+  '%c env',
+  'color: lightyellow; font-size: large',
+  process.env.NODE_ENV,
+  process.env.DEBUG,
+  process.env.IS_BROWSER
+)
 
 render(createElement(App), document.getElementById('app'))
