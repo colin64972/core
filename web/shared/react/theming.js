f3ed322fc1ccb855b604e3c5d2f8dc563399ad46
colin30 <colin30@post.com>
2019-12-08 19:15:55 -0800

    add keyword multiplier notfound route and theme

---
+++ web/shared/react/theming.js
@@ -0,0 +1,248 @@
+import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles'
+import fail from '@material-ui/core/colors/red'
+import pass from '@material-ui/core/colors/lime'
+import warn from '@material-ui/core/colors/amber'
+
+export const defaultPadding = (breakpoints, setSpace, scale) => {
+  if (scale)
+    return {
+      padding: setSpace(),
+      [breakpoints.up('sm')]: {
+        padding: setSpace('sm')
+      },
+      [breakpoints.up('md')]: {
+        padding: `${setSpace('md') * scale}px ${setSpace('md')}px`
+      },
+      [breakpoints.up('lg')]: {
+        padding: `${setSpace('lg') * scale}px ${setSpace('lg')}px`
+      },
+      [breakpoints.up('xl')]: {
+        padding: `${setSpace('xl') * scale}px ${setSpace('xl')}px`
+      }
+    }
+  return {
+    padding: setSpace(),
+    [breakpoints.up('sm')]: {
+      padding: setSpace('sm')
+    },
+    [breakpoints.up('md')]: {
+      padding: setSpace('md')
+    },
+    [breakpoints.up('lg')]: {
+      padding: setSpace('lg')
+    },
+    [breakpoints.up('xl')]: {
+      padding: setSpace('xl')
+    }
+  }
+}
+
+export const setCustomTheme = (
+  fontSize,
+  fontFamilyName,
+  fontFaces,
+  primary,
+  secondary,
+  bodyColor,
+  borderRadius,
+  factor = 3
+) => {
+  const fontFamily = [
+    fontFamilyName,
+    '"Helvetica Neue"',
+    'Arial',
+    'serif'
+  ].join(',')
+  const headingStyle = {
+    fontWeight: 'bold',
+    textTransform: 'uppercase',
+    lineHeight: 1
+  }
+  const muiTheme = createMuiTheme({
+    palette: {
+      primary,
+      secondary,
+      pass,
+      warn,
+      fail,
+      bodyColor
+    },
+    typography: {
+      fontSize,
+      htmlFontSize: fontSize,
+      fontFamily,
+      italic: {
+        fontStyle: 'italic'
+      },
+      bold: {
+        fontWeight: 'bold'
+      },
+      boldItalic: {
+        fontWeight: 'bold',
+        fontStyle: 'italic'
+      }
+    },
+    custom: {
+      setSpace: breakpoint => {
+        switch (breakpoint) {
+          case 'sm':
+            return fontSize * 2
+          case 'md':
+            return fontSize * 4
+          case 'lg':
+            return fontSize * 8
+          case 'xl':
+            return fontSize * 16
+          default:
+            return fontSize
+        }
+      },
+      cleanList: {
+        listStyle: 'none',
+        padding: 0,
+        margin: 0
+      },
+      setGrid: (columnCount, rowCount, gapSpace = 0) => ({
+        display: 'grid',
+        gridTemplateColumns: `repeat(${columnCount}, 1fr)`,
+        gridTemplateRows:
+          rowCount === 'auto' ? 'auto' : `repeat(${rowCount}, 1fr)`,
+        gridRowGap: gapSpace,
+        gridColumnGap: gapSpace
+      }),
+      setFlex: (flow = 'row nowrap', justify = 'center', align = 'center') => ({
+        display: 'flex',
+        flexFlow: flow,
+        justifyContent: justify,
+        alignItems: align
+      }),
+      buttons: {
+        base: {
+          'border': 'none',
+          'backgroundColor': 'transparent',
+          'color': bodyColor,
+          'padding': 0,
+          'margin': 0,
+          'cursor': 'pointer',
+          'fontFamily': fontFamily,
+          fontSize,
+          'transition': 'all 250ms ease-out',
+          '&:focus': {
+            outline: 'none'
+          }
+        },
+        form: (disabled, background) => ({
+          'width': '100%',
+          'padding': fontSize,
+          borderRadius,
+          'border': 'none',
+          '&:focus': {
+            outline: 'none'
+          },
+          'color': disabled ? 'rgb(158, 158, 158)' : bodyColor,
+          'cursor': !disabled && 'pointer',
+          'fontFamily': fontFamily,
+          fontSize,
+          'fontWeight': 'bold',
+          'textTransform': 'uppercase',
+          background,
+          'transition': 'all 250ms ease-out',
+          'display': 'flex',
+          'flexFlow': 'row nowrap',
+          'justifyContent': 'center',
+          'alignItems': 'center'
+        })
+      },
+      borderRadius
+    },
+    debug: {
+      border: {
+        border: '1px solid red'
+      }
+    },
+    props: {
+      MuiTypography: {
+        variantMapping: {
+          h1: 'h1',
+          h2: 'h2',
+          h3: 'h3',
+          h4: 'h4',
+          h5: 'h5',
+          h6: 'h6',
+          subtitle1: 'h2',
+          subtitle2: 'h2',
+          body1: 'p',
+          body2: 'p'
+        }
+      }
+    },
+    overrides: {
+      MuiCssBaseline: {
+        '@global': {
+          '@font-face': fontFaces,
+          'body': {
+            fontFamily,
+            color: bodyColor,
+            textAlign: 'center',
+            backgroundColor: 'white'
+          }
+        }
+      },
+      MuiTypography: {
+        h1: {
+          fontSize: fontSize * 4,
+          ...headingStyle
+        },
+        h2: {
+          fontSize: fontSize * 2,
+          ...headingStyle
+        },
+        h3: {
+          fontSize: fontSize * 1.75,
+          ...headingStyle
+        },
+        h4: {
+          fontSize: fontSize * 1.5,
+          ...headingStyle
+        },
+        h5: {
+          fontSize: fontSize * 1.25,
+          ...headingStyle
+        },
+        h6: {
+          fontSize,
+          ...headingStyle
+        },
+        subtitle1: {
+          fontSize: fontSize * 2,
+          lineHeight: 1
+        },
+        subtitle2: {
+          fontSize: fontSize * 2,
+          lineHeight: 1,
+          fontStyle: 'italic'
+        }
+      },
+      MuiTableRow: {
+        root: {
+          'borderTop': `1px solid ${secondary[100]}`,
+          'thead &': {
+            border: 'none'
+          }
+        }
+      },
+      MuiTableCell: {
+        sizeSmall: {
+          'textAlign': 'center',
+          'color': bodyColor,
+          'border': 'none',
+          'padding': fontSize / 2,
+          '&:last-child': {
+            paddingRight: fontSize / 2
+          }
+        }
+      }
+    }
+  })
+  return responsiveFontSizes(muiTheme, { factor })
+}
