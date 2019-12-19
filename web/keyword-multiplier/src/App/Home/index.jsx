da31deb81b86bf42ed067c034b8f1e6285f1b457
colin30 <colin30@post.com>
2019-12-18 22:56:33 -0800

    add back notice bar

--- web/keyword-multiplier/src/App/Home/index.jsx
+++ web/keyword-multiplier/src/App/Home/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Header from './Header'
 import Intro from './Intro'
-import Sets from './Sets'
 import MatchTypes from './MatchTypes'
+import Notice from './Notice'
+import Sets from './Sets'
 import Trials from './Trials'
 import Footer from '../common/Footer'
 
 const Home = () => (
   <div id="home-container">
+    <Notice />
     <Header />
     <Intro />
     <Sets />
