e1dc64b6c7d0f88c0e30baf39d99ab1a24aef504
colin64972 <56013989+colin64972@users.noreply.github.com>
2020-01-01 16:40:30 -0800

    reorganize web into clients folder, update files

---
+++ clients/web/keywordMultiplier/src/App/routes.js
@@ -0,0 +1,23 @@
+import { generateKey } from '@colin30/clients-web-shared/react'
+import Home from './Home'
+import NotFound from './NotFound'
+import constants from './constants'
+
+const routes = [
+  {
+    key: generateKey(),
+    path: constants.URLS.HOME,
+    exact: true,
+    component: Home,
+    label: 'Home'
+  },
+  {
+    key: generateKey(),
+    path: constants.URLS.NOT_FOUND,
+    exact: false,
+    component: NotFound,
+    label: 'Not Found'
+  }
+]
+
+export default routes
