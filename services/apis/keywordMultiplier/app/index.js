6a62935e2c935e850049e7e76160873710034d09
colin64972 <56013989+colin64972@users.noreply.github.com>
2020-01-10 14:29:27 -0800

    add test sam service

--- services/apis/keywordMultiplier/app/index.js
+++ services/apis/keywordMultiplier/app/index.js
@@ -1,7 +1,12 @@
+require('dotenv').config()
 const express = require('express')
+const mongoose = require('mongoose')
+const { connectDb } = require('@colin30/services-shared/express/db')
 const trialsRouter = require('./trials/router')
 
 const app = express()
+
+connectDb(process.env.DB_NAME, mongoose.connect)
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
