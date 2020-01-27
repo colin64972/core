ca89cddec3fa6253ada51e252307f71129f5a1a0
cjo3 <56013989+cjo3@users.noreply.github.com>
2020-01-26 17:02:51 -0800

    remove most services, add scripts to catcher package

--- services/apis/keywordMultiplier/index.js
+++
@@ -1,5 +0,0 @@
-const { proxy, createServer } = require('aws-serverless-express')
-const { setSamHandler } = require('@colin30/services-shared/sam')
-const app = require('./app')
-
-exports.handler = setSamHandler(app, proxy, createServer)
