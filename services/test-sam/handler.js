8bc6a4229b24f6779fbdf569272659b762c3c5b7
colin64972 <56013989+colin64972@users.noreply.github.com>
2020-01-26 17:02:51 -0800

    remove most services, add scripts to catcher package

--- services/test-sam/handler.js
+++
@@ -1,5 +0,0 @@
-const { proxy, createServer } = require('aws-serverless-express')
-const { setSamHandler } = require('@colin30/services-shared/sam')
-const app = require('./app')
-
-exports.main = setSamHandler(app, proxy, createServer)
