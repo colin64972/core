0c0e959a9dd39256ab22ec1a4a63b15c748d7c7f
colin30 <colin30@post.com>
2020-02-23 11:23:58 -0800

    remove express app, aws-serverless-express, serverless-http

--- serverless/renderer/index.js
+++ serverless/renderer/index.js
@@ -1,4 +1,20 @@
-import serverless from 'serverless-http'
-import app from './app'
+export const scan = (event, context, callback) => {
+  const result = [
+    {
+      id: 'asdf',
+      ipAddress: '1.2.4.5',
+      sets: {
+        set2: ['hello', 'goodbye'],
+        set4: ['sally', 'jim']
+      }
+    }
+  ]
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(result)
+  }
 
-export const renderer = serverless(app)
+  console.log(event, context, callback)
+
+  return callback(null, response)
+}
