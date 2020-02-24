c429f190ee659b4c36a04a28a4b40127f5728869
colin64972 <56013989+colin64972@users.noreply.github.com>
2020-02-24 11:30:46 -0800

    rename scan function to getAll

--- serverless/renderer/index.js
+++ serverless/renderer/index.js
@@ -1,20 +1,22 @@
-export const scan = (event, context, callback) => {
-  const result = [
-    {
-      id: 'asdf',
-      ipAddress: '1.2.4.5',
-      sets: {
-        set2: ['hello', 'goodbye'],
-        set4: ['sally', 'jim']
+export const getAll = async (event, context, callback) => {
+  try {
+    const result = [
+      {
+        id: 'asdf',
+        ipAddress: '1.2.4.5',
+        sets: {
+          set2: ['hello', 'goodbye'],
+          set4: ['sally', 'jim']
+        }
       }
+    ]
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(result)
     }
-  ]
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify(result)
+
+    return callback(null, response)
+  } catch (err) {
+    console.log(err)
   }
-
-  console.log(event, context, callback)
-
-  return callback(null, response)
 }
