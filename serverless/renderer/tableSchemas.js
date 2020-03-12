5ceb58817cc78f073b76845a161bdd3d3fd29175
cjo3 <56013989+cjo3@users.noreply.github.com>
2020-03-12 14:48:22 -0700

    add js table manager and yarn scripts to manipulate dynamo tables

---
+++ serverless/renderer/tableSchemas.js
@@ -0,0 +1,51 @@
+module.exports = {
+  rendersTableSchema: {
+    TableName: process.env.RENDERS_TABLE_NAME,
+    AttributeDefinitions: [
+      {
+        AttributeName: 'id',
+        AttributeType: 'S'
+      },
+      {
+        AttributeName: 'domain',
+        AttributeType: 'S'
+      },
+      {
+        AttributeName: 'path',
+        AttributeType: 'S'
+      }
+    ],
+    KeySchema: [
+      {
+        AttributeName: 'id',
+        KeyType: 'HASH'
+      }
+    ],
+    GlobalSecondaryIndexes: [
+      {
+        IndexName: process.env.RENDERS_TABLE_GLOBAL_SECONDARY_INDEX_NAME,
+        Projection: {
+          ProjectionType: 'KEYS_ONLY'
+        },
+        KeySchema: [
+          {
+            AttributeName: 'domain',
+            KeyType: 'HASH'
+          },
+          {
+            AttributeName: 'path',
+            KeyType: 'RANGE'
+          }
+        ],
+        ProvisionedThroughput: {
+          WriteCapacityUnits: 1,
+          ReadCapacityUnits: 5
+        }
+      }
+    ],
+    ProvisionedThroughput: {
+      WriteCapacityUnits: 1,
+      ReadCapacityUnits: 5
+    }
+  }
+}
