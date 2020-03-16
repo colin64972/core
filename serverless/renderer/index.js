9a7a4588782fcacc62ebc8d5f9b3b1c836b1c8f8
colin30 <colin30@post.com>
2020-03-15 21:01:10 -0700

    change primary key to domain and path, getOne to getByPrimaryKey

--- serverless/renderer/index.js
+++ serverless/renderer/index.js
@@ -1,7 +1,7 @@
 import controller from './controller'
 
 export const createOne = async (event, context, callback) => {
-  const slsRes = await controller.createOne(event.body, context.awsRequestId)
+  const slsRes = await controller.createOne(event.body)
   return callback(null, slsRes)
 }
 
--- serverless/renderer/tableSchemas.js
+++ serverless/renderer/tableSchemas.js
@@ -5,40 +5,40 @@ module.exports = {
     TableName: process.env.RENDERS_TABLE_NAME,
     AttributeDefinitions: [
       {
-        AttributeName: 'id',
-        AttributeType: 'S'
-      },
-      {
         AttributeName: 'domain',
         AttributeType: 'S'
       },
       {
         AttributeName: 'path',
         AttributeType: 'S'
+      },
+      {
+        AttributeName: 'id',
+        AttributeType: 'S'
       }
     ],
     KeySchema: [
       {
-        AttributeName: 'id',
+        AttributeName: 'domain',
         KeyType: 'HASH'
+      },
+      {
+        AttributeName: 'path',
+        KeyType: 'RANGE'
       }
     ],
     GlobalSecondaryIndexes: [
       {
         IndexName: process.env.RENDERS_TABLE_GLOBAL_SECONDARY_INDEX_NAME,
+        KeySchema: [
+          {
+            AttributeName: 'id',
+            KeyType: 'HASH'
+          }
+        ],
         Projection: {
           ProjectionType: 'KEYS_ONLY'
         },
-        KeySchema: [
-          {
-            AttributeName: 'domain',
-            KeyType: 'HASH'
-          },
-          {
-            AttributeName: 'path',
-            KeyType: 'RANGE'
-          }
-        ],
         ProvisionedThroughput: {
           WriteCapacityUnits: 1,
           ReadCapacityUnits: 5
