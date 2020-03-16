7dcb8c2447cc81f9867b817464a9ffd448501c14
cjo3 <56013989+cjo3@users.noreply.github.com>
2020-03-15 21:01:10 -0700

    change primary key to domain and path, getOne to getByPrimaryKey

---
+++ serverless/shared/constants.js
@@ -0,0 +1,13 @@
+const constants = {
+  ERRORS: {
+    DYNAMODB: {
+      NO_ITEMS: {
+        ERROR_CODE: 'JL88GQV4000000004016543941787648',
+        STATUS_CODE: 400,
+        MESSAGE: 'no items'
+      }
+    }
+  }
+}
+
+module.exports = constants
