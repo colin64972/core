a5193ceb293831a8df50789ba6b9d071631ca792
cjo3 <56013989+cjo3@users.noreply.github.com>
2020-04-04 14:29:13 -0700

    remove local invoke events, switch getAll and getOne methods, update routes

--- projects/keywordMultiplier/api/index.js
+++ projects/keywordMultiplier/api/index.js
@@ -1,5 +1,10 @@
 import controller from './controller'
 
+export const createOne = async (event, context, callback) => {
+  const slsRes = await controller.createOne(event.body)
+  return callback(null, slsRes)
+}
+
 export const getAll = async (event, context, callback) => {
   const slsRes = await controller.getAll()
   return callback(null, slsRes)
