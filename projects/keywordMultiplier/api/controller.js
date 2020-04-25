e6e11d5f4711ff6b0a56a78adc2aaca0cc8cc2ff
colin64972 <56013989+colin64972@users.noreply.github.com>
2020-04-24 20:14:55 -0700

    add create one to keyword multiplier api

--- projects/keywordMultiplier/api/controller.js
+++ projects/keywordMultiplier/api/controller.js
@@ -1,47 +1,30 @@
 import AWS from 'aws-sdk'
-import { v4 as uuidv4 } from 'uuid'
-import setDynamoConstants from '@colin30/shared/constants/dynamodb'
+import { constants, helpers } from '@colin30/shared'
 
-const dynamoConstants = setDynamoConstants()
+const { dynamoDbConstants } = constants
+const { createHashId } = helpers
 
 const dbOptions = {}
 
 if (process.env.IS_LOCAL || process.env.IS_OFFLINE) {
-  dbOptions.region = dynamoConstants.LOCAL.REGION
-  dbOptions.endpoint = dynamoConstants.LOCAL.ENDPOINT
+  dbOptions.region = dynamoDbConstants.LOCAL.REGION
+  dbOptions.endpoint = dynamoDbConstants.LOCAL.ENDPOINT
 }
 
 const docClient = new AWS.DynamoDB.DocumentClient(dbOptions)
 
-const serviceError = error => ({
-  statusCode: 500,
-  body: JSON.stringify({
-    name: 'service error',
-    error
-  })
-})
-
 const createOne = async eventBody => {
-  let response, body
-
   try {
-    body = eventBody
-
-    if (process.env.IS_OFFLINE) {
-      const parsed = JSON.parse(eventBody)
-      body = {
-        ...parsed
-      }
-    }
+    let parsed = JSON.parse(eventBody)
 
     const timestamp = new Date().getTime()
 
     const options = {
       TableName: process.env.TABLE_NAME,
       Item: {
-        id: uuidv4(),
-        ipAddress: body.ipAddress,
-        sets: body.sets,
+        id: createHashId(),
+        ipAddress: parsed.ipAddress,
+        sets: parsed.sets,
         createdAt: timestamp,
         updatedAt: timestamp
       }
@@ -49,37 +32,15 @@ const createOne = async eventBody => {
 
     await docClient.put(options).promise()
 
-    response = {
+    return {
       statusCode: 201,
       body: JSON.stringify(options.Item)
     }
   } catch (error) {
-    console.log('DYNAMO ERROR', error.message)
-    response = serviceError(error)
+    return proxyServerError(error)
   }
-  return response
-}
-
-const getAll = async () => {
-  let response
-  try {
-    const result = await docClient
-      .scan({
-        TableName: process.env.TABLE_NAME
-      })
-      .promise()
-    response = {
-      statusCode: 200,
-      body: JSON.stringify(result.Items)
-    }
-  } catch (error) {
-    console.log('DYNAMO ERROR', error.message)
-    response = serviceError(error)
-  }
-  return response
 }
 
 export default {
-  createOne,
-  getAll
+  createOne
 }
