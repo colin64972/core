2694c9e6375f97b422d22bd9180b6db0a5161a21
colin30 <colin30@post.com>
2020-04-24 20:14:55 -0700

    add create one to keyword multiplier api

--- projects/keywordMultiplier/api/index.js
+++ projects/keywordMultiplier/api/index.js
@@ -4,8 +4,3 @@ export const createOne = async (event, context, callback) => {
   const slsRes = await controller.createOne(event.body)
   return callback(null, slsRes)
 }
-
-export const getAll = async (event, context, callback) => {
-  const slsRes = await controller.getAll()
-  return callback(null, slsRes)
-}
