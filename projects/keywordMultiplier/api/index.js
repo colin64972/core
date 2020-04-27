135641e38fb0b97edb0c43fa6d5c91a81bb43d8e
colin30 <colin30@post.com>
2020-04-26 20:23:16 -0700

    add get one by path param id

--- projects/keywordMultiplier/api/index.js
+++ projects/keywordMultiplier/api/index.js
@@ -9,3 +9,8 @@ export const getAll = async (event, context, callback) => {
   const slsRes = await controller.getAll(event.queryStringParameters)
   return callback(null, slsRes)
 }
+
+export const getOne = async (event, context, callback) => {
+  const slsRes = await controller.getOne(event.pathParameters)
+  return callback(null, slsRes)
+}
