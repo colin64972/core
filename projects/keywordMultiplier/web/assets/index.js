094c89623a6f8bd1b9cc981ade6392baed991c79
cjo3 <56013989+cjo3@users.noreply.github.com>
2020-05-18 22:29:12 -0700

    reduce pixel density image levels to 3

--- projects/keywordMultiplier/web/assets/index.js
+++ projects/keywordMultiplier/web/assets/index.js
@@ -1,20 +1,18 @@
-import Intro1x from './images/intro-300w.png'
-import Intro2x from './images/intro-600w.png'
-import Intro3x from './images/intro-900w.png'
-import Intro4x from './images/intro-1200w.png'
+import Intro1x from './images/intro-400w.png'
+import Intro2x from './images/intro-800w.png'
+import Intro3x from './images/intro-1200w.png'
 import ColinProfile1x from './images/colin30-profile-48w.jpg'
 import ColinProfile2x from './images/colin30-profile-96w.jpg'
 import ColinProfile3x from './images/colin30-profile-144w.jpg'
-import ColinProfile4x from './images/colin30-profile-192w.jpg'
 
 export const Intro = {
   alt: 'content-marketing-seo-illustration',
   format: 'png',
-  paths: [Intro1x, Intro2x, Intro3x, Intro4x]
+  paths: [Intro1x, Intro2x, Intro3x]
 }
 
 export const ColinProfile = {
   alt: 'colin30-profile-photo',
   format: 'jpg',
-  paths: [ColinProfile1x, ColinProfile2x, ColinProfile3x, ColinProfile4x]
+  paths: [ColinProfile1x, ColinProfile2x, ColinProfile3x]
 }
