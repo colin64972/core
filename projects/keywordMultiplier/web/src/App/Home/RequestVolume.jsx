c6ebe8f4ba894a9f6fda38d9860514509998730f
colin30 <colin30@post.com>
2020-05-24 20:53:40 -0700

    move react dev deps to configs, add dotenv vars to webpack, start request volume modal

--- projects/keywordMultiplier/web/src/App/Home/RequestVolume.jsx
+++ projects/keywordMultiplier/web/src/App/Home/RequestVolume.jsx
@@ -1,22 +1,46 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Modal from '@material-ui/core/Modal'
 import Grid from '@material-ui/core/Grid'
+import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/styles'
+import { defaultPadding } from '@colin30/shared/react/theming'
 
-const useStyles = makeStyles(theme => ({}))
+const useStyles = makeStyles(theme => ({
+  fullHeight: {
+    height: '100vh'
+  },
+  paper: {
+    ...defaultPadding(theme.breakpoints, theme.custom.setSpace),
+    [theme.breakpoints.down('sm')]: {
+      margin: `0 ${theme.custom.setSpace()}px`
+    }
+  }
+}))
 
 const RequestVolume = ({ status, closeHandler, trial }) => {
   const classes = useStyles()
-
+  useEffect(() => {
+    closeHandler()
+  })
   return (
     <Modal open={status} onClose={closeHandler}>
       <Grid container justify="center" alignItems="center">
-        <Grid item>
-          <Typography variant="h2">Hello</Typography>
-          <Typography variant="body1">
-            {JSON.stringify(trial, null, 2)}
-          </Typography>
+        <Grid item xs={12}>
+          <Grid
+            container
+            justify="center"
+            alignItems="center"
+            className={classes.fullHeight}>
+            <Grid item xs={12} md={10} lg={8}>
+              <Paper elevation={1} className={classes.paper}>
+                <Typography variant="h3">Volume Request Options</Typography>
+                <Typography variant="body1">
+                  {JSON.stringify(trial, null, 2)}
+                </Typography>
+              </Paper>
+            </Grid>
+          </Grid>
         </Grid>
       </Grid>
     </Modal>
