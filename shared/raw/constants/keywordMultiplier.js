012d952505fb3e2a1bc14affb82e83c753f92b9e
colin64972 <56013989+colin64972@users.noreply.github.com>
2020-06-14 11:10:10 -0700

    add endpoint constants, update api to fetch based on queryStringParams

---
+++ shared/raw/constants/keywordMultiplier.js
@@ -0,0 +1,14 @@
+const creditsPath = 'v1/account/credits'
+const countriesPath = 'v1/countries'
+const currenciesPath = 'v1/currencies'
+const keywordsPath = 'v1/get_keyword_data'
+
+exports.KeConstants = {
+  ENDPOINTS: {
+    meta: [creditsPath, countriesPath, currenciesPath],
+    credits: [creditsPath],
+    countries: [countriesPath],
+    currencies: [currenciesPath],
+    keywords: [keywordsPath]
+  }
+}
