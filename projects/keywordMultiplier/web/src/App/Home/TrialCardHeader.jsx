a77ae5c3d195cb995c99e4f0b78d809c47a5826d
colin64972 <56013989+colin64972@users.noreply.github.com>
2020-06-27 23:49:01 -0700

    add entry count and date to trial card header

--- projects/keywordMultiplier/web/src/App/Home/TrialCardHeader.jsx
+++ projects/keywordMultiplier/web/src/App/Home/TrialCardHeader.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Chip from '@material-ui/core/Chip'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import AssignmentIcon from '@material-ui/icons/Assignment'
@@ -9,6 +10,9 @@ const useStyles = makeStyles(theme => ({
   trialCardHeading: {
     textAlign: 'left',
     textTransform: 'unset',
+    margin: `${theme.custom.setSpace('sm')}px 0 ${
+      theme.custom.setSpace() / 4
+    }px 0`,
     [theme.breakpoints.down('xs')]: {
       fontSize: theme.custom.setSpace() * 1.75
     }
@@ -18,8 +22,14 @@ const useStyles = makeStyles(theme => ({
     textAlign: 'left',
     textTransform: 'unset'
   },
+  copyDeleteContainer: {
+    ...theme.custom.setFlex(),
+    position: 'relative',
+    right: -33
+  },
   copyButton: {
     ...theme.custom.iconButton,
+    'margin': `0 ${theme.custom.setSpace() / 2}px 0 0`,
     'color': theme.palette.primary[50],
     'backgroundColor': theme.palette.primary[200],
     '&:hover': {
@@ -29,7 +39,7 @@ const useStyles = makeStyles(theme => ({
   },
   deleteButton: {
     ...theme.custom.iconButton,
-    'marginRight': 0,
+    'margin': 0,
     'color': theme.palette.secondary[50],
     'backgroundColor': theme.palette.secondary[200],
     '&:hover': {
@@ -40,8 +50,19 @@ const useStyles = makeStyles(theme => ({
   actionButtonIcon: {
     fontSize: theme.custom.setSpace() * 1.5
   },
-  cardInfo: {
-    marginTop: theme.custom.setSpace()
+  listCount: {
+    ...theme.typography.bold,
+    color: theme.palette.primary[50],
+    backgroundColor: theme.palette.primary[200],
+    marginRight: theme.custom.setSpace() / 2,
+    height: 30
+  },
+  createdAt: {
+    height: 30
+  },
+  chipLabel: {
+    position: 'relative',
+    top: 1
   }
 }))
 
@@ -54,21 +75,39 @@ export const TrialCardHeader = ({
   return (
     <Grid container>
       <Grid item xs={12}>
-        <Grid container justify="flex-start" alignItems="center">
-          <button onClick={copyHandler} className={classes.copyButton}>
-            <AssignmentIcon className={classes.actionButtonIcon} />
-          </button>
-          <button
-            type="button"
-            onClick={askDeleteTrialHandler}
-            className={classes.deleteButton}>
-            <DeleteIcon className={classes.actionButtonIcon} />
-          </button>
+        <Grid container justify="space-between" alignItems="flex-start">
+          <Grid item>
+            <Chip
+              label={trial.list.length}
+              className={classes.listCount}
+              classes={{
+                label: classes.chipLabel
+              }}
+            />
+            <Chip
+              label={trial.createdAt}
+              className={classes.createdAt}
+              classes={{
+                label: classes.chipLabel
+              }}
+            />
+          </Grid>
+          <Grid item className={classes.copyDeleteContainer}>
+            <button onClick={copyHandler} className={classes.copyButton}>
+              <AssignmentIcon className={classes.actionButtonIcon} />
+            </button>
+            <button
+              type="button"
+              onClick={askDeleteTrialHandler}
+              className={classes.deleteButton}>
+              <DeleteIcon className={classes.actionButtonIcon} />
+            </button>
+          </Grid>
         </Grid>
       </Grid>
-      <Grid item xs={12} className={classes.cardInfo}>
+      <Grid item xs={10}>
         <Typography variant="h6" className={classes.trialCardHeading}>
-          {trial.heading}
+          Set Fields {trial.heading}
         </Typography>
         <Typography variant="body1" className={classes.trialCardHeadingId}>
           {trial.id}
