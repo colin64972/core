00eaf00b98acd3d11322de963920a2dc10020d4d
colin30 <colin30@post.com>
2020-07-04 23:42:27 -0700

    replace modal for dialog, add keyword review modal

--- projects/keywordMultiplier/web/src/App/Home/RequestVolume.jsx
+++ projects/keywordMultiplier/web/src/App/Home/RequestVolume.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Formik } from 'formik'
 import { FadeIn } from '@colin30/shared/react/components/FadeIn'
-import Fade from '@material-ui/core/Fade'
-import Backdrop from '@material-ui/core/Backdrop'
-import Grid from '@material-ui/core/Grid'
-import Modal from '@material-ui/core/Modal'
-import Paper from '@material-ui/core/Paper'
+import Dialog from '@material-ui/core/Dialog'
 import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/styles'
 import { defaultPadding } from '@colin30/shared/react/theming'
@@ -14,21 +10,10 @@ import { RequestVolumeForm } from './RequestVolumeForm'
 import { constants } from '../constants'
 
 const useStyles = makeStyles(theme => ({
-  fullHeight: {
-    height: '100vh'
-  },
   paper: {
+    ...theme.custom.setFlex('column', 'center', 'flex-start'),
     ...defaultPadding(theme.breakpoints, theme.custom.setSpace),
-    backgroundColor: theme.palette.grey[200],
-    [theme.breakpoints.down('md')]: {
-      margin: `0 ${theme.custom.setSpace('md')}px`
-    },
-    [theme.breakpoints.down('sm')]: {
-      margin: `0 ${theme.custom.setSpace('sm')}px`
-    },
-    [theme.breakpoints.down('xs')]: {
-      margin: `0 ${theme.custom.setSpace('xs')}px`
-    }
+    backgroundColor: theme.palette.secondary[100]
   },
   subHeading: {
     width: '100%',
@@ -38,15 +23,10 @@ const useStyles = makeStyles(theme => ({
     ...theme.typography.mainHeading,
     width: '100%',
     textAlign: 'left'
-  },
-  body1: {
-    width: '100%',
-    textAlign: 'left',
-    margin: `${theme.custom.setSpace()}px 0`
   }
 }))
 
-const RequestVolume = ({ status, modalCloseHandler, trial }) => {
+const RequestVolume = ({ drawerStatus, closeDrawerHandler, trial }) => {
   const classes = useStyles()
 
   const countryOptions = useSelector(state => state.kE.countries)
@@ -75,49 +55,42 @@ const RequestVolume = ({ status, modalCloseHandler, trial }) => {
   }
 
   return (
-    <Modal
-      open={status}
-      onClose={modalCloseHandler}
-      closeAfterTransition
-      BackdropComponent={Backdrop}
-      BackdropProps={{
-        timeout: 500
+    <Dialog
+      open={drawerStatus}
+      transitionDuration={500}
+      disableBackdropClick
+      disableEscapeKeyDown
+      fullScreen
+      PaperProps={{
+        classes: {
+          root: classes.paper
+        }
       }}>
-      <Fade in={status}>
-        <Grid
-          container
-          justify="center"
-          alignItems="center"
-          className={classes.fullHeight}>
-          <Paper elevation={1} className={classes.paper}>
-            <FadeIn direction="x" position={-100}>
-              <Typography variant="subtitle2" className={classes.subHeading}>
-                Order Form
-              </Typography>
-            </FadeIn>
-            <FadeIn direction="x" position={-100}>
-              <Typography variant="h4" className={classes.mainHeading}>
-                Keyword Volume Metrics
-              </Typography>
-            </FadeIn>
-            <Formik initialValues={initalValues} onSubmit={customSubmitHandler}>
-              {formikProps => (
-                <RequestVolumeForm
-                  trial={trial}
-                  modalCloseHandler={modalCloseHandler}
-                  kEOptions={{
-                    countryOptions,
-                    currencyOptions,
-                    dataSourceOptions
-                  }}
-                  formikProps={formikProps}
-                />
-              )}
-            </Formik>
-          </Paper>
-        </Grid>
-      </Fade>
-    </Modal>
+      <FadeIn direction="x" position={-100}>
+        <Typography variant="subtitle2" className={classes.subHeading}>
+          Order Form
+        </Typography>
+      </FadeIn>
+      <FadeIn direction="x" position={-100}>
+        <Typography variant="h4" className={classes.mainHeading}>
+          Keyword Volume Metrics
+        </Typography>
+      </FadeIn>
+      <Formik initialValues={initalValues} onSubmit={customSubmitHandler}>
+        {formikProps => (
+          <RequestVolumeForm
+            trial={trial}
+            closeDrawerHandler={closeDrawerHandler}
+            kEOptions={{
+              countryOptions,
+              currencyOptions,
+              dataSourceOptions
+            }}
+            formikProps={formikProps}
+          />
+        )}
+      </Formik>
+    </Dialog>
   )
 }
 
