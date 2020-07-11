3d7ed456040bf75662e588f13369fc93e2f2d1b9
colin30 <colin30@post.com>
2020-07-11 14:48:58 -0700

    add fade in to card info and pricing

--- projects/keywordMultiplier/web/src/App/Home/VolumeFormPricing.jsx
+++ projects/keywordMultiplier/web/src/App/Home/VolumeFormPricing.jsx
@@ -23,35 +23,38 @@ export const VolumeFormPricing = ({ formSectionClass }) => {
 
   return (
     <Paper className={formSectionClass}>
-      <Field name="acceptTerms">
-        {fieldProps => {
-          // console.log(
-          //   '%c fieldProps',
-          //   'color: lightgreen; font-size: large',
-          //   fieldProps
-          // )
-          return (
-            <FormControl
-              required
-              // error={error}
-              // component="fieldset"
-              // className={classes.formControl}
-            >
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    // checked={}
-                    // onChange={handleChange}
-                    name={fieldProps.field.name}
-                  />
-                }
-                label="I accept the Terms of Service"
-              />
-              <FormHelperText>You can display an error</FormHelperText>
-            </FormControl>
-          )
-        }}
-      </Field>
+      <FadeIn direction="x" position={Math.random() > 0.5 ? 100 : -100}>
+        <Field name="acceptTerms">
+          {fieldProps => {
+            // console.log(
+            //   '%c fieldProps',
+            //   'color: lightgreen; font-size: large',
+            //   fieldProps
+            // )
+            return (
+              <FormControl
+                required
+                fullWidth
+                // error={error}
+                // component="fieldset"
+                // className={classes.formControl}
+              >
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      // checked={}
+                      // onChange={handleChange}
+                      name={fieldProps.field.name}
+                    />
+                  }
+                  label="I accept the Terms of Service"
+                />
+                <FormHelperText>You can display an error</FormHelperText>
+              </FormControl>
+            )
+          }}
+        </Field>
+      </FadeIn>
     </Paper>
   )
 }
