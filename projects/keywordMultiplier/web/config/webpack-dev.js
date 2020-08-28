b977f5903c92af7a91a1bf9b55c769d99fcbaba8
cjo3 <56013989+cjo3@users.noreply.github.com>
2020-08-27 17:16:58 -0700

    replace stripe keys with env setup

--- projects/keywordMultiplier/web/config/webpack-dev.js
+++ projects/keywordMultiplier/web/config/webpack-dev.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const { EnvironmentPlugin } = require('webpack')
 const { merge } = require('webpack-merge')
 const { config } = require('./webpack')
@@ -5,7 +6,8 @@ const { config } = require('./webpack')
 config.plugins.push(
   new EnvironmentPlugin({
     DEBUG: true,
-    IS_BROWSER: true
+    IS_BROWSER: true,
+    STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY_TEST
   })
 )
 
