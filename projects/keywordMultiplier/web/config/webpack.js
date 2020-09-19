4f447d0167408ac62dc6825d804bf2cf9c323d64
colin64972 <56013989+colin64972@users.noreply.github.com>
2020-09-18 22:47:11 -0700

    update terms of service useing props from parent

--- projects/keywordMultiplier/web/config/webpack.js
+++ projects/keywordMultiplier/web/config/webpack.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const { setConfig } = require('@colin30/configs/react')
+const { EnvironmentPlugin } = require('webpack')
 
 const baseConfig = setConfig(
   { src: path.resolve('src', 'index') },
@@ -17,6 +18,14 @@ const baseConfig = setConfig(
   {
     title: process.env.npm_package_config_template_title
   }
+)
+
+baseConfig.plugins.push(
+  new EnvironmentPlugin({
+    SITE_NAME: process.env.SITE_NAME,
+    SITE_URL: process.env.SITE_URL,
+    SITE_CONTACT: process.env.SITE_CONTACT
+  })
 )
 
 exports.config = baseConfig
