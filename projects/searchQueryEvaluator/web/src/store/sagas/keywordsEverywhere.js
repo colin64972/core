d4d53a926853a88c3f42cef91e59eaacfa62cb2d
cjo3 <56013989+cjo3@users.noreply.github.com>
2020-10-06 15:48:24 -0700

    rename keyword multiplier to search query evalutator

---
+++ projects/searchQueryEvaluator/web/src/store/sagas/keywordsEverywhere.js
@@ -0,0 +1,159 @@
+import { call, put, select, take, race, delay } from 'redux-saga/effects'
+import {
+  fetchKeData,
+  makePreOrder,
+  fetchKeVolumes,
+  postLowCreditAlert
+} from '../fetchers'
+import { types } from '../types'
+import {
+  decorateKeOptions,
+  decorateTrial,
+  generateNotice
+} from '@colin30/shared/logic/searchQueryEvaluator'
+import { constants } from '@colin30/shared/raw/constants/searchQueryEvaluator'
+import { payloadMock } from '@colin30/shared/react/mocks/searchQueryEvaluator'
+
+export function* getKeOptions() {
+  try {
+    const { countries, currencies } = yield select(state => state.kE)
+    if (!countries || !currencies) {
+      const result = yield call(
+        fetchKeData,
+        Object.keys(constants.ENDPOINTS)[0]
+      )
+      const decoratedData = decorateKeOptions(result.data)
+      return yield put({
+        type: types.SET_KE_OPTIONS,
+        ...decoratedData
+      })
+    }
+    return null
+  } catch (error) {
+    // TODO
+  }
+}
+
+export function* getKeCredits() {
+  try {
+    const result = yield call(fetchKeData, Object.keys(constants.ENDPOINTS)[1])
+    const credits = result?.data.credits[0]
+
+    if (
+      credits < constants.LOW_CREDIT_ALERT_THRESHOLD &&
+      !process.env.USE_MOCKS
+    ) {
+      yield call(postLowCreditAlert, credits)
+    }
+
+    yield put({
+      type: types.SET_KE_CREDITS,
+      credits
+    })
+  } catch (error) {
+    console.error('%c getKeCredits', 'color: red; font-size: large', error)
+  }
+}
+
+export function* orderMetrics(action) {
+  yield put({
+    type: types.SET_SPINNER_STATUS,
+    spinnerName: constants.VOLUME_SPINNER,
+    status: true
+  })
+  const notice = generateNotice('Metrics purchased successfully')
+
+  let stripeError = null
+
+  try {
+    const orderRequest = yield select(state => state.kE.orderRequest)
+
+    const preOrderRes = yield call(
+      makePreOrder,
+      orderRequest,
+      action.values.country,
+      action.values.currency,
+      action.values.dataSource
+    )
+
+    const { client_secret } = preOrderRes
+
+    let payload
+
+    if (process.env.USE_MOCKS) {
+      payload = payloadMock
+    } else {
+      payload = yield call(action.confirmCardPaymentHandler, client_secret, {
+        receipt_email: action.values.billingEmail || null,
+        payment_method: {
+          card: action.cardNumberElement,
+          billing_details: {
+            email: action.values.billingEmail || null
+          }
+        }
+      })
+    }
+
+    if (payload.error) {
+      stripeError = payload.error
+      throw Error('stripeError', payload.error)
+    }
+
+    const keVolumes = yield call(
+      fetchKeVolumes,
+      orderRequest.trialId,
+      payload.paymentIntent.id,
+      action.values.country,
+      action.values.currency,
+      action.values.dataSource
+    )
+
+    yield put({
+      type: types.SET_KE_CREDITS,
+      credits: keVolumes.credits
+    })
+
+    const updatedTrial = decorateTrial(keVolumes.updatedTrial)
+
+    yield put({
+      type: types.UPDATE_TRIAL,
+      updatedTrial
+    })
+    yield put({
+      type: types.SET_SPINNER_STATUS,
+      spinnerName: constants.VOLUME_SPINNER,
+      status: false
+    })
+    return action.closeDialogHandler()
+  } catch (error) {
+    console.error('%c FAIL', 'color: red; font-size: large', error, stripeError)
+    action.closeDialogHandler()
+    notice.bg = constants.NOTICE.BGS.FAIL
+    notice.heading = 'Payment Error'
+    notice.message = 'Please try again later'
+  } finally {
+    yield put({
+      type: types.ADD_USER_KE_SELECTIONS,
+      country: action.values.country,
+      currency: action.values.currency,
+      dataSource: action.values.dataSource
+    })
+    yield put({
+      type: types.SET_SPINNER_STATUS,
+      spinnerName: constants.ORDER_SPINNER,
+      status: true
+    })
+    yield put({
+      type: types.ADD_NOTICE,
+      notice
+    })
+    yield put({ type: types.SHOW_NOTICE })
+    yield race({
+      response: take(types.TAKE_NOTICE_RESPONSE),
+      timeout: delay(constants.NOTICE.TIMEOUT_DELAY)
+    })
+    yield put({ type: types.HIDE_NOTICE })
+    yield delay(500)
+    yield put({ type: types.REMOVE_NOTICE })
+  }
+}
