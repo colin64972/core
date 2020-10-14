3f0abb9df908c7e8e0674bb2fe6d9d66986049a1
cjo3 <56013989+cjo3@users.noreply.github.com>
2020-10-14 15:23:07 -0700

    remove name from footer image

--- projects/km/web/assets/index.js
+++ projects/km/web/assets/index.js
@@ -1,18 +1,18 @@
 import Intro1x from './images/intro-400w.png'
 import Intro2x from './images/intro-800w.png'
 import Intro3x from './images/intro-1200w.png'
-import ColinProfile1x from './images/colin-profile-48w.jpg'
-import ColinProfile2x from './images/colin-profile-96w.jpg'
-import ColinProfile3x from './images/colin-profile-144w.jpg'
+import ProfilePic1x from './images/profile-pic-48w.jpg'
+import ProfilePic2x from './images/profile-pic-96w.jpg'
+import ProfilePic3x from './images/profile-pic-144w.jpg'
 
 export const Intro = {
-  alt: 'content-marketing-seo-illustration',
+  alt: 'online-marketing-seo-illustration',
   mimeType: 'image/png',
   paths: [Intro1x, Intro2x, Intro3x]
 }
 
-export const ColinProfile = {
-  alt: 'colin-profile-photo',
+export const ProfilePic = {
+  alt: 'profile-photo',
   mimeType: 'image/jpeg',
-  paths: [ColinProfile1x, ColinProfile2x, ColinProfile3x]
+  paths: [ProfilePic1x, ProfilePic2x, ProfilePic3x]
 }
