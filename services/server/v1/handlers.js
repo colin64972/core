059d7badeb8b52029be0fc8ec2ecfee0b81acc81
cjo3 <56013989+cjo3@users.noreply.github.com>
2020-10-21 22:58:08 -0700

    setup server to pull s3 object, parse buffer to html, send html response

--- services/server/v1/handlers.js
+++ services/server/v1/handlers.js
@@ -1,8 +1,48 @@
-import middy from '@middy/core'
-import jsonBodyParser from '@middy/http-json-body-parser'
+import AWS from 'aws-sdk'
 
-export const serveRoot = middy(async (event, context, callback) => {
-  console.log('event', event)
-  let slsRes = {}
+const s3 = new AWS.S3({
+  apiVersion: '2006-03-01',
+  region: process.env.REGION,
+  sslEnabled: true
+})
+
+const fetchAndRenderPage = async routeKey => {
+  console.log('fetchAndRenderPage', routeKey)
+
+  try {
+    const objectListRes = await s3
+      .listObjectsV2({
+        Bucket: process.env.CDN_BUCKET_NAME
+      })
+      .promise()
+
+    const indexObject = await s3
+      .getObject({
+        Bucket: process.env.CDN_BUCKET_NAME,
+        Key: 'index.html'
+      })
+      .promise()
+
+    const html = indexObject.Body.toString()
+
+    return {
+      statusCode: 200,
+      headers: {
+        'content-type': 'text/html'
+      },
+      body: html
+    }
+  } catch (error) {
+    console.error('fetchAndRenderPage', 'FAIL', error)
+    return {
+      status: 500,
+      body: JSON.stringify(error)
+    }
+  }
+}
+
+export const servePageHandler = async (event, context, callback) => {
+  const html = await fetchAndRenderPage(event.routeKey)
+  return context.succeed(html)
   return callback(null, slsRes)
-}).use(jsonBodyParser())
+}
