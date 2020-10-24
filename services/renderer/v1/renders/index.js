f5b618b363101ddb72bb2c4e58155c0dc2327bda
cjo3 <56013989+cjo3@users.noreply.github.com>
2020-10-24 14:02:54 -0700

    re add renderer service with get all and create render

---
+++ services/renderer/v1/renders/index.js
@@ -0,0 +1,63 @@
+import AWS from 'aws-sdk'
+import { dynamoDbConstants } from '@cjo3/shared/raw/constants/dynamoDb'
+
+const dbOptions = {
+  dynamodb: '2012-08-10'
+}
+
+if (process.env.IS_LOCAL || process.env.IS_OFFLINE) {
+  dbOptions.region = dynamoDbConstants.LOCAL.REGION
+  dbOptions.endpoint = dynamoDbConstants.LOCAL.ENDPOINT
+}
+
+const docClient = new AWS.DynamoDB.DocumentClient(dbOptions)
+
+export const createRender = async eventBody => {
+  try {
+    const { app, path, head, body } = eventBody
+
+    const timestamp = new Date().getTime()
+
+    const options = {
+      TableName: process.env.RENDERS_TABLE_NAME,
+      Item: {
+        app,
+        path,
+        head,
+        body,
+        createdAt: timestamp,
+        updatedAt: timestamp
+      }
+    }
+
+    await docClient.put(options).promise()
+
+    return {
+      statusCode: 201,
+      body: JSON.stringify(options.Item)
+    }
+  } catch (error) {
+    console.error('createRender', error)
+  }
+}
+
+export const getRenders = async () => {
+  try {
+    const params = {
+      TableName: process.env.RENDERS_TABLE_NAME
+    }
+
+    const result = await docClient.scan(params).promise()
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(result.Items)
+    }
+  } catch (error) {
+    console.error('getRenders', error)
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error, null, 2)
+    }
+  }
+}
