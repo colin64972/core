eb81eb06f01d2ada7231274ffc360c48590bb4a5
cjo3 <56013989+cjo3@users.noreply.github.com>
2020-10-27 17:09:03 -0700

    complete test edge router to redirect www and non-https to https only

--- projects/edge-router/v1/handlers.js
+++ projects/edge-router/v1/handlers.js
@@ -1,4 +1,3 @@
-import { fetchPage } from './fetchers'
 import { WWW_HOST } from '@cjo3/shared/raw/constants/regex'
 
 export const rootHandler = async (event, context, callback) => {
@@ -8,13 +7,58 @@ export const rootHandler = async (event, context, callback) => {
 
   host = host[0].value
 
-  if (WWW_HOST.test(host))
-    return {
-      status: 301,
-      headers: {
-        location: `https://${process.env.HOST}${uri}`
-      }
-    }
+  console.log('LOG rootHandler', host, uri, querystring, method)
 
-  return fetchPage(host, uri, querystring, method)
+  if (WWW_HOST.test(host))
+    return callback(null, {
+      status: '301',
+      statusDescription: 'redirect',
+      headers: {
+        location: [
+          {
+            key: 'Location',
+            value: `https://${process.env.HOST}${uri}`
+          }
+        ]
+      }
+    })
+
+  const content = `
+    <\!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <title>Simple Lambda@Edge Static Content Response</title>
+      </head>
+      <body>
+        <h1>Req Params</h1>
+        <p><strong>method</strong>${method}</p>
+        <p><strong>host</strong>${host}</p>
+        <p><strong>uri</strong>${uri}</p>
+        <p><strong>querystring</strong>${querystring}</p>
+      </body>
+    </html>
+    `
+
+  const response = {
+    status: '200',
+    statusDescription: 'OK',
+    headers: {
+      'cache-control': [
+        {
+          key: 'Cache-Control',
+          value: 'max-age=100'
+        }
+      ],
+      'content-type': [
+        {
+          key: 'Content-Type',
+          value: 'text/html'
+        }
+      ]
+    },
+    body: content
+  }
+
+  return callback(null, response)
 }
