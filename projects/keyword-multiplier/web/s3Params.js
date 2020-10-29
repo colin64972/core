7f366dcb2c52554b1f9d3505483d887d774e6263
cjo3 <56013989+cjo3@users.noreply.github.com>
2020-10-29 15:50:45 -0700

    update all envKeys, update react s3 sync scripts and params

--- projects/keyword-multiplier/web/s3Params.js
+++ projects/keyword-multiplier/web/s3Params.js
@@ -1,25 +1,13 @@
 require('dotenv').config()
 
-exports.createParams = {
-  // dryrun: false,
-  bucketName: process.env.CDN_BUCKET,
-  region: process.env.S3_REGION,
-  acl: 'public-read'
-}
-
-exports.webParams = {
+exports.web = {
   // dryrun: false,
   srcPath: 'dist',
   s3Path: `${process.env.CDN_BUCKET}/${process.env.CDN_APP_FOLDER}`,
   excludes: ['*.html']
 }
 
-exports.deleteParams = {
-  // dryrun: false,
-  keyPath: `${process.env.CDN_BUCKET}/${process.env.CDN_APP_FOLDER}`
-}
-
-exports.preRendersParams = {
+exports.preRenders = {
   // dryrun: false,
   srcPath: 'distPreRenders',
   s3Path: `${process.env.PRERENDERS_BUCKET}`,
