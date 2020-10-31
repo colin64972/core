bfb12b7a31a3e83c56f874bd168701d1232bcde5
cjo3 <56013989+cjo3@users.noreply.github.com>
2020-10-31 01:33:46 -0700

    update clean script

--- clean.sh
+++ clean.sh
@@ -2,14 +2,6 @@
 
 . ~/.bash_functions --source-only
 
-function printGreen {
-  echo -e "\033[32m $1 \033[0m"
-}
-
-function printYellow {
-  echo -e "\033[93m $1 \033[0m"
-}
-
 printYellow Cleaning...
 
 deleteFolders node_modules dist distPreRenders .serverless
