8d248baf4bb7f9ab4534a4370932cacfcf8323cb
cjo3 <56013989+cjo3@users.noreply.github.com>
2020-11-03 22:27:57 -0800

    add eslint with react and typescript

---
+++ projects/dle-web/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true
+    },
+    sourceType: 'module'
+  },
+  settings: {
+    react: {
+      version: 'detect'
+    }
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'no-loops'
+  ],
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  rules: {
+    'no-undef': 'off',
+    'no-loops/no-loops': 'warn'
+  }
+}
--- projects/dle-web/src/index.ts
+++ projects/dle-web/src/index.ts
@@ -3,3 +3,7 @@ import { createElement } from 'react'
 import { render } from 'react-dom'
 
 render(createElement(App), document.getElementById('app'));
+
+for (let i = 0; i < 10; i += 1) {
+  console.log('asdf')
+}
