d6fa0404eece80029cebdac9103b9774c17f4bb4
cjo3 <56013989+cjo3@users.noreply.github.com>
2020-11-03 22:44:17 -0800

    add prettier wiith format on save vscode

--- projects/dle-web/src/index.ts
+++ projects/dle-web/src/index.ts
@@ -1,8 +1,8 @@
-import {App} from './App'
+import { App } from './App'
 import { createElement } from 'react'
 import { render } from 'react-dom'
 
-render(createElement(App), document.getElementById('app'));
+render(createElement(App), document.getElementById('app'))
 
 for (let i = 0; i < 10; i += 1) {
   console.log('asdf')
