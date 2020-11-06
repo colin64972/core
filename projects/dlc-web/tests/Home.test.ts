ac749948e2f49835c023bae7c085504dad7d73dd
cjo3 <56013989+cjo3@users.noreply.github.com>
2020-11-05 16:33:55 -0800

    add test for homepage

---
+++ projects/dlc-web/tests/Home.test.ts
@@ -0,0 +1,40 @@
+import { testRenderer } from './testRenderer'
+import { Home } from '../src/App/Home'
+
+describe('Home', () => {
+  describe('render', () => {
+    test('renders heading', () => {
+      const { getByRole } = testRenderer('/', Home)
+      const heading = getByRole('heading', { level: 1 })
+      expect(heading).toBeInTheDocument()
+    })
+
+    test('renders copy', () => {
+      const { getByText } = testRenderer('/', Home)
+      const copy = getByText('consetetur', {
+        selector: 'p',
+        exact: false
+      })
+      expect(copy).toBeInTheDocument()
+    })
+
+    test('renders link to editor', () => {
+      const { getByRole } = testRenderer('/', Home)
+      const startLink = getByRole('link', { name: /start/i })
+      expect(startLink).toHaveAttribute('href', '/editor')
+    })
+  })
+
+  describe('events', () => {
+    test('link clicks to editor', () => {
+      const {
+        getByRole,
+        userEvent: { click }
+      } = testRenderer('/')
+      const startLink = getByRole('link', { name: /start/i })
+      click(startLink)
+      const heading = getByRole('heading', { level: 1 })
+      expect(heading.innerHTML).toBe('Editor')
+    })
+  })
+})
