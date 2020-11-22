312bd2da13bd371dd83eec93638549d24a3cac8b
cjo3 <56013989+cjo3@users.noreply.github.com>
2020-11-22 13:19:57 -0800

    add style prefix to fix prod style rule conflicts

--- projects/dle-web/src/ThemedApp.ts
+++ projects/dle-web/src/ThemedApp.ts
@@ -1,10 +1,19 @@
-import { ThemeProvider } from '@material-ui/core/styles'
+import {
+  StylesProvider,
+  ThemeProvider,
+  createGenerateClassName
+} from '@material-ui/core/styles'
 import { createElement } from 'react'
 import { App } from './App'
 import { theme } from './theme'
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'dle-style-',
+  seed: 'my-jss'
+})
+
 export const ThemedApp = createElement(
-  ThemeProvider,
-  { theme },
-  createElement(App)
+  StylesProvider,
+  { injectFirst: false, generateClassName },
+  createElement(ThemeProvider, { theme }, createElement(App))
 )
