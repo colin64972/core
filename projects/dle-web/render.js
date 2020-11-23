2cd16d0989f6d212c0f5b0ec6ec8ff1bb876e22d
cjo3 <56013989+cjo3@users.noreply.github.com>
2020-11-23 14:39:30 -0800

    add in render test from old branch

---
+++ projects/dle-web/render.js
@@ -0,0 +1,35 @@
+require('colors')
+const fs = require('fs')
+const path = require('path')
+const { createElement } = require('react')
+const { Provider } = require('react-redux')
+const { StaticRouter } = require('react-router-dom')
+const { ServerStyleSheets } = require('@material-ui/core/styles')
+const { renderToString } = require('react-dom/server')
+
+let file = fs
+  .readFileSync(path.resolve('distPreRenders', 'ThemedApp.js'))
+  .toString()
+let evalled = eval(file)
+const { ThemedApp } = evalled
+
+file = fs.readFileSync(path.resolve('distPreRenders', 'store.js')).toString()
+evalled = eval(file)
+const { setReduxStore } = evalled
+const store = setReduxStore()
+
+const sheets = new ServerStyleSheets()
+
+try {
+  const asdf = createElement(
+    StaticRouter,
+    { location: '/apps/detection-limit-editor', context: {} },
+    createElement(Provider, { store }, ThemedApp)
+  )
+
+  const render = renderToString(sheets.collect(asdf))
+
+  console.log('LOG XXX'.yellow, sheets.toString())
+} catch (error) {
+  console.error('ERROR XXX'.red, error)
+}
---
+++ projects/dle-web/webpack.render.js
@@ -0,0 +1,120 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const { EnvironmentPlugin } = require('webpack')
+const { setPreRenderFilePublicPath } = require('@cjo3/shared/raw/general')
+const ImageminWebpWebpackPlugin = require('imagemin-webp-webpack-plugin')
+const path = require('path')
+
+const localEnv = require('dotenv').config()
+const sharedEnv = require('dotenv').config({
+  path: path.resolve('..', 'shared', '.env')
+})
+
+module.exports = {
+  entry: {
+    ThemedApp: path.resolve('src', 'ThemedApp.ts'),
+    store: path.resolve('src', 'store', 'index.ts')
+  },
+  devtool: process.env.NODE_ENV === 'development' ? 'source-map' : null,
+  mode: process.env.NODE_ENV,
+  output: {
+    path: path.resolve('distPreRenders'),
+    filename: '[name].js',
+    libraryTarget: 'commonjs2'
+  },
+  target: 'node',
+  resolve: {
+    extensions: [
+      '.css',
+      '.gif',
+      '.jpg',
+      '.js',
+      '.json',
+      '.jsx',
+      '.png',
+      '.sass',
+      '.svg',
+      '.ts',
+      '.tsx'
+    ]
+  },
+  externals: [
+    'canvas',
+    'react',
+    'react-redux',
+    'react-router',
+    /@material-ui\/.*/gi
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-typescript',
+              '@babel/preset-react',
+              [
+                '@babel/preset-env',
+                {
+                  targets: {
+                    node: 'current'
+                  },
+                  modules: 'commonjs'
+                }
+              ]
+            ]
+          }
+        }
+      },
+      {
+        test: /\.(woff(2)?|jpg|gif|png|svg|ico)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              emitFile: false,
+              name: '[folder]/[name].[ext]',
+              publicPath: '/'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin({ verbose: true }),
+    new EnvironmentPlugin({
+      IS_NOT_SERVER: false,
+      ACCEPTED_FILETYPES: localEnv.parsed.ACCEPTED_FILETYPES,
+      API_URL: localEnv.parsed.API_URL,
+      APP_NAME: localEnv.parsed.APP_NAME,
+      APP_ROOT_PATH: localEnv.parsed.APP_ROOT_PATH,
+      AUTH_SECRET: sharedEnv.parsed.AUTH_SECRET,
+      CDN_APP_FOLDER: localEnv.parsed.CDN_APP_FOLDER,
+      EXPORT_PRICE: localEnv.parsed.EXPORT_PRICE,
+      JWT_PRIVATE_KEY: sharedEnv.parsed.JWT_PRIVATE_KEY,
+      SITE_CONTACT_EMAIL: localEnv.parsed.SITE_CONTACT_EMAIL,
+      SITE_NAME: localEnv.parsed.SITE_NAME,
+      SITE_URL: localEnv.parsed.SITE_URL,
+      // STRIPE_PUBLIC_KEY: localEnv.parsed.STRIPE_PUBLIC_KEY_LIVE,
+      STRIPE_PUBLIC_KEY: localEnv.parsed.STRIPE_PUBLIC_KEY_TEST,
+      STRIPE_URL: localEnv.parsed.STRIPE_URL
+    })
+    // new ImageminWebpWebpackPlugin({
+    //   config: [
+    //     {
+    //       test: /\.(jpe?g|png)/,
+    //       options: {
+    //         quality: 75
+    //       }
+    //     }
+    //   ],
+    //   overrideExtension: true,
+    //   detailedLogs: false,
+    //   silent: false,
+    //   strict: true
+    // })
+  ]
+}
