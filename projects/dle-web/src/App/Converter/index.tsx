5e3e54e121c301d6165c3674c897bea443640ee9
cjo3 <56013989+cjo3@users.noreply.github.com>
2020-11-25 00:20:45 -0800

    fix scroller no target error

--- projects/dle-web/src/App/Converter/index.tsx
+++ projects/dle-web/src/App/Converter/index.tsx
@@ -1,36 +1,13 @@
-import { Header } from '../Header'
-import { ConverterBg } from '../../assets'
 import { Grid } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
 import React from 'react'
+import { ConverterBg } from '../../assets'
+import { Header } from '../Header'
 import { FileLoader } from './FileLoader'
 import { SheetSelector } from './SheetSelector'
 import { TransformResults } from './TransformResults'
 import { TransformSettings } from './TransformSettings'
 
-const useStyles = makeStyles(
-  theme => ({
-    Converter_headerContent: {
-      ...theme.custom.setFlex('column'),
-      padding: theme.custom.setSpace('sm'),
-      color: theme.palette.grey[50],
-      textShadow: theme.custom.textShadow
-    },
-    Converter_headerInstructionsButton: {
-      marginTop: theme.custom.setSpace('sm')
-    },
-    Converter_contentContainer: {
-      ...theme.custom.contentContainer
-    }
-  }),
-  {
-    name: 'Converter'
-  }
-)
-
 export const Converter: React.FC = (): JSX.Element => {
-  const classes = useStyles()
-
   return (
     <Grid container justify="center">
       <Header
