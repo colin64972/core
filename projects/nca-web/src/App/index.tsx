c0ff6ff86526e86764900ed1b9ac76fc07294436
cjo3 <56013989+cjo3@users.noreply.github.com>
2020-12-01 14:42:36 -0800

    add material ui packages

--- projects/nca-web/src/App/index.tsx
+++ projects/nca-web/src/App/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
+import ScheduleIcon from '@material-ui/icons/Schedule'
+import { Grid, Typography } from '@material-ui/core'
 
 export const App: React.FC = (): JSX.Element => (
-  <div>
-    <h1>{process.env.APP_NAME}</h1>
-    <p>
+  <Grid>
+    <Typography variant="h1">{process.env.APP_NAME}</Typography>
+    <ScheduleIcon />
+    <Typography variant="body1">
       Ipsum justo nonumy vero kasd magna nonumy et. Gubergren sit diam elitr no
       nonumy takimata est, stet lorem sadipscing et amet nonumy duo. Accusam
       vero takimata ut lorem diam, et dolores dolor kasd dolor dolore. Vero est
       et et vero justo eos stet aliquyam dolores. Elitr elitr erat et sea.
-    </p>
-  </div>
+    </Typography>
+  </Grid>
 )
