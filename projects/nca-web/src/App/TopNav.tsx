ce174a986ecef3e93181a14d09cb08132ab51c71
cjo3 <56013989+cjo3@users.noreply.github.com>
2020-12-10 16:03:24 -0800

    replace resize handler with mui usemediaquery for topnav

--- projects/nca-web/src/App/TopNav.tsx
+++ projects/nca-web/src/App/TopNav.tsx
@@ -1,5 +1,5 @@
 import NcaLogoWhite from '@cjo3/shared/assets/svgs/nca-logo-white'
-import { Grid } from '@material-ui/core'
+import { Grid, useMediaQuery } from '@material-ui/core'
 import { makeStyles } from '@material-ui/core/styles'
 import MenuIcon from '@material-ui/icons/Menu'
 import React, { useState } from 'react'
@@ -25,16 +25,12 @@ const useStyles = makeStyles(
   }
 )
 
-interface Props {
-  viewWidth: number
-}
-
-export const TopNav: React.FC<Props> = ({ viewWidth }): JSX.Element => {
+export const TopNav: React.FC = (): JSX.Element => {
   const classes = useStyles()
 
   const [sideMenuOpen, setSideMenuOpen] = useState<boolean>(false)
 
-  const showSideMenuIcon = viewWidth < 768
+  const matches = useMediaQuery('(max-width: 599px)')
 
   const openSideMenuHandler = (): void => {
     setSideMenuOpen(true)
@@ -48,7 +44,7 @@ export const TopNav: React.FC<Props> = ({ viewWidth }): JSX.Element => {
       <ContentContainer bgColor="theme.palette.primary.main">
         <Grid container justify="space-between" alignItems="center">
           <img src={NcaLogoWhite} className={classes.logo} />
-          {showSideMenuIcon ? (
+          {matches ? (
             <MenuIcon
               className={classes.sideMenuIcon}
               onClick={openSideMenuHandler}
@@ -64,7 +60,7 @@ export const TopNav: React.FC<Props> = ({ viewWidth }): JSX.Element => {
             />
           )}
         </Grid>
-        {showSideMenuIcon && sideMenuOpen && (
+        {matches && sideMenuOpen && (
           <SideMenu open={sideMenuOpen} closeHandler={closeSideMenuHandler} />
         )}
       </ContentContainer>
--- projects/nca-web/src/App/index.tsx
+++ projects/nca-web/src/App/index.tsx
@@ -1,6 +1,5 @@
 import { CssBaseline } from '@material-ui/core'
-import React, { useLayoutEffect, useState } from 'react'
-import { useEffect } from 'react'
+import React from 'react'
 import { Route, Switch, useLocation } from 'react-router-dom'
 import { Apps } from './Apps/'
 import { Contact } from './Contact/'
@@ -9,58 +8,21 @@ import { Home } from './Home'
 import { NotFound } from './NotFound'
 import { Resume } from './Resume/'
 import { TopNav } from './TopNav'
-import { getContent } from './fetchers'
 
 export const App: React.FC = (): JSX.Element => {
   const location = useLocation()
+
   const showNav = /(resume|apps|contact)\/?$/i.test(location.pathname)
-  const [viewWidth, udpateViewWidth] = useState<number>(window.innerWidth)
-  const [siteContent, updateSiteContent] = useState({})
-
-  const resizeHandler = () => {
-    udpateViewWidth(window.innerWidth)
-  }
-
-  if (!process.env.IS_SERVER) {
-    useLayoutEffect(() => {
-      window.addEventListener('resize', resizeHandler)
-      return () => window.removeEventListener('resize', resizeHandler)
-    })
-  }
-
-  useEffect(() => {
-    getContent(window.location.pathname)
-      .then(pageContent => {
-        updateSiteContent({
-          ...siteContent,
-          [pageContent.path]: pageContent.content
-        })
-      })
-      .catch(error =>
-        console.error('%c getContent', 'color: red; font-size: large', error)
-      )
-  }, [location])
 
   return (
     <CssBaseline>
-      {showNav && <TopNav viewWidth={viewWidth} />}
+      {showNav && <TopNav />}
       <Switch>
-        <Route
-          path="/"
-          exact
-          render={() => <Home content={siteContent['/']} />}
-        />
+        <Route path="/" exact component={Home} />
         <Route path="/resume" exact component={Resume} />
         <Route path="/apps" exact component={Apps} />
-        <Route
-          path="/contact"
-          exact
-          render={() => <Contact content={siteContent['/contact']} />}
-        />
-        <Route
-          path="/*"
-          render={() => <NotFound content={siteContent['/error']} />}
-        />
+        <Route path="/contact" exact component={Contact} />
+        <Route path="/*" component={NotFound} />
       </Switch>
       <Footer />
     </CssBaseline>
