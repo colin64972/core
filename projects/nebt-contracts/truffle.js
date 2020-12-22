8534b6cb99dce8b7cbf6e784538716c4f0fe63f0
cjo3 <56013989+cjo3@users.noreply.github.com>
2020-12-22 15:28:26 -0800

    update contracts env vars

--- projects/nebt-contracts/truffle.js
+++ projects/nebt-contracts/truffle.js
@@ -8,9 +8,9 @@ module.exports = {
     development: {
       host: process.env.LOCAL_HOST,
       port: process.env.LOCAL_PORT,
-      network_id: '5777',
+      network_id: process.env.LOCAL_NETWORK_ID,
     },
-    rinkeby: {
+    [process.env.TESTNET_NAME]: {
       provider: () =>
         new HDWalletProvider(
           process.env.PRIVATE_KEYS.split(','),
@@ -18,7 +18,7 @@ module.exports = {
           0,
           3
         ),
-      network_id: 4,
+      network_id: process.env.TESTNET_ID,
       gas: 6500000,
       gasPrice: 100000000000,
     },
