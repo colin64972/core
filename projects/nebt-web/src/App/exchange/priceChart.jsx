e91dafc37c0ce703519b8a4486f661e2c4f266a8
cjo3 <56013989+cjo3@users.noreply.github.com>
2020-12-22 16:51:49 -0800

    update price chart size

--- projects/nebt-web/src/App/exchange/priceChart.jsx
+++ projects/nebt-web/src/App/exchange/priceChart.jsx
@@ -4,7 +4,6 @@ import { makeStyles } from '@material-ui/styles'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import Chart from 'react-apexcharts'
-import { config } from './chartConfig'
 import ArrowIcon from '../components/arrowIcon'
 import { selectChartTrades, selectCurrentPrice } from '../../store/selectors'
 
@@ -30,8 +29,80 @@ const useStyles = makeStyles(theme => ({
   subtitle: {
     fontWeight: 'normal',
     color: theme.palette.grey[600]
+  },
+  chart: {
+    width: 520,
+    [theme.breakpoints.up('md')]: {
+      width: 880
+    },
+    [theme.breakpoints.up('lg')]: {
+      width: 950
+    }
   }
 }))
+
+const chartOptions = {
+  chart: {
+    animations: { enabled: false },
+    toolbar: { show: false }
+  },
+  tooltip: {
+    enabled: true,
+    theme: false,
+    style: {
+      color: 'white',
+      fontSize: '12px',
+      fontFamily: undefined
+    },
+    x: {
+      show: false,
+      format: 'dd MMM',
+      formatter: undefined
+    },
+    y: {
+      show: true,
+      title: 'price'
+    },
+    marker: {
+      show: false
+    },
+    items: {
+      display: 'flex'
+    },
+    fixed: {
+      enabled: false,
+      position: 'topRight',
+      offsetX: 0,
+      offsetY: 0
+    }
+  },
+  xaxis: {
+    type: 'datetime',
+    labels: {
+      show: true,
+      style: {
+        colors: '#fff',
+        fontSize: '8px',
+        cssClass: 'apexcharts-xaxis-label'
+      }
+    }
+  },
+  yaxis: {
+    labels: {
+      show: true,
+      minWidth: 0,
+      maxWidth: 160,
+      style: {
+        color: '#fff',
+        fontSize: '8px',
+        cssClass: 'apexcharts-yaxis-label'
+      },
+      offsetX: 0,
+      offsetY: 0,
+      rotate: 0
+    }
+  }
+}
 
 export default ({ ...props }) => {
   const classes = useStyles()
@@ -39,24 +110,21 @@ export default ({ ...props }) => {
   let currentPrice = useSelector(state => selectCurrentPrice(state))
   if (!trades) return 'No Data'
   return (
-    <Grid container>
-      <Grid item xs={12}>
-        <Typography variant="h4" className={classes[currentPrice.direction]}>
-          <ArrowIcon direction={currentPrice.direction} />
-          {currentPrice.value}
-        </Typography>
-        <Typography variant="body1" className={classes.subtitle}>
-          ETH / NEB
-        </Typography>
-      </Grid>
-      <Grid item xs={12}>
-        <Chart
-          options={config}
-          series={trades}
-          type="candlestick"
-          width="100%"
-        />
-      </Grid>
+    <Grid>
+      <Typography variant="h4" className={classes[currentPrice.direction]}>
+        <ArrowIcon direction={currentPrice.direction} />
+        {currentPrice.value}
+      </Typography>
+      <Typography variant="body1" className={classes.subtitle}>
+        ETH / NEB
+      </Typography>
+      <Chart
+        options={chartOptions}
+        series={trades}
+        type="candlestick"
+        className={classes.chart}
+        height="277px"
+      />
     </Grid>
   )
 }
